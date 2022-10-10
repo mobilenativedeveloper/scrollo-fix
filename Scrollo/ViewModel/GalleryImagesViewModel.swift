d7b2fe8144c975e91ce9e93355dfd1aebd4091a7
Strelnik Artem <gipatskiyartem@gmail.com>
2022-10-10 10:19:40 +0300

    fix

---
+++ Scrollo/ViewModel/GalleryImagesViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  GalleryImagesViewModel.swift
+//  Scrollo
+//
+//  Created by Artem Strelnik on 09.10.2022.
+//
+
+import SwiftUI
+
+struct GalleryImagesViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GalleryImagesViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        GalleryImagesViewModel()
+    }
+}
